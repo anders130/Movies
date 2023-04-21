9fd4b9c40f52e9e5b419df15122ae2d9c65fda19
Jesse Gollub <jesse.gollub@ewe.net>
2023-04-21 10:19:19 +0200

    #11 added FastEndpoints

--- Movies.Api/Program.cs
+++ Movies.Api/Program.cs
@@ -1,8 +1,10 @@
+global using FastEndpoints;
 using Microsoft.EntityFrameworkCore;
 using Movies.Api.Data;
 using Movies.Api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddFastEndpoints();
 
 // Add services to the container.
 builder.Services.AddDbContext<MovieDbContext>(options =>
@@ -12,5 +14,7 @@ builder.Services.AddSingleton<IMovieRepository, MovieRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseAuthorization();
+app.UseFastEndpoints();
 
 app.Run();
