9aeb09e734967f9d90850b607bdf35063563954b
Jesse Gollub <jesse.gollub@ewe.net>
2023-04-22 13:07:10 +0200

    #15 added CreateMovie
    used OneOf for discriminated unions

--- Movies.Api/Services/IMovieRepository.cs
+++ Movies.Api/Services/IMovieRepository.cs
@@ -1,8 +1,11 @@
 ﻿using Movies.Api.Models;
+using OneOf;
+using OneOf.Types;
 
 namespace Movies.Api.Services;
 
 public interface IMovieRepository
 {
     List<Movie> GetMovies();
+    OneOf<int, AlreadyExists> CreateMovie(Movie movie);
 }
--- Movies.Api/Services/MovieRepository.cs
+++ Movies.Api/Services/MovieRepository.cs
@@ -1,5 +1,7 @@
 ﻿using Movies.Api.Data;
 using Movies.Api.Models;
+using OneOf;
+using OneOf.Types;
 
 namespace Movies.Api.Services;
 
@@ -15,5 +17,15 @@ public class MovieRepository : IMovieRepository
     public List<Movie> GetMovies()
     {
         return _context.Movies.ToList();
+    }
+
+    public OneOf<int, AlreadyExists> CreateMovie(Movie movie)
+    {
+        if (movie.Id != 0 && _context.Movies.Find(movie.Id) is not null)
+            return new AlreadyExists();
+
+        _context.Movies.Add(movie);
+        _context.SaveChanges();
+        return movie.Id;
     }
 }
