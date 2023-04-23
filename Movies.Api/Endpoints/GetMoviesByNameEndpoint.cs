4a985061f34b9958ae3910884e6038919f04958b
Jesse Gollub <jesse.gollub@ewe.net>
2023-04-23 18:35:58 +0200

    #26 added GetMoviesByNameEndpoint

---
+++ Movies.Api/Endpoints/GetMoviesByNameEndpoint.cs
@@ -0,0 +1,27 @@
+﻿using Movies.Api.Models;
+using Movies.Api.Requests;
+using Movies.Api.Services;
+
+namespace Movies.Api.Endpoints;
+
+public sealed class GetMoviesByNameEndpoint : Endpoint<GetMoviesByNameRequest, List<Movie>>
+{
+    private readonly IMovieRepository _movieRepository;
+
+    public GetMoviesByNameEndpoint(IMovieRepository movieRepository)
+    {
+        _movieRepository = movieRepository;
+    }
+
+    public override void Configure()
+    {
+        Get("movies/name/{name}");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(GetMoviesByNameRequest req, CancellationToken ct)
+    {
+        var movies = _movieRepository.GetMoviesByName(req.Name);
+        await SendAsync(movies, cancellation: ct);
+    }
+}
